da9c1a9f267ccc6a99f4658deffb7f552e1a4489
Ajurna <ajurna@gmail.com>
2023-10-16 12:02:53 +0100

    day 21 finished. i had the defence items costs wrong >.<

--- day21/01.py
+++ day21/01.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Union, List, Tuple, Iterable
+from typing import Iterable
 
 
 @dataclass
@@ -26,7 +26,6 @@ class Equipment:
 
 
 weapons = [
-    Equipment('None', cost=0),
     Equipment('Dagger', cost=8, damage=4),
     Equipment('Shortsword', cost=10, damage=5),
     Equipment('Warhammer', cost=25, damage=6),
@@ -47,9 +46,9 @@ rings = [
     Equipment('Damage +1', cost=25, damage=1),
     Equipment('Damage +2', cost=50, damage=2),
     Equipment('Damage +3', cost=100, damage=3),
-    Equipment('Defense +1', cost=25, armor=1),
-    Equipment('Defense +2', cost=50, armor=2),
-    Equipment('Defense +3', cost=100, armor=3),
+    Equipment('Defense +1', cost=20, armor=1),
+    Equipment('Defense +2', cost=40, armor=2),
+    Equipment('Defense +3', cost=80, armor=3),
 ]
 sets = [(a, b, c, d) for a in weapons for b in armors for c in rings for d in rings if c != d]
 
@@ -75,7 +74,6 @@ for e_set in sets:
     player = Player()
     player.add_equipment(e_set)
     enemy_boss = Player(health=103, damage=9, defence=2)
-    print()
     if e_set[0].name == 'None':
         continue
     while True:
@@ -85,9 +83,6 @@ for e_set in sets:
         enemy_boss.attack(player)
         if player.health <= 0:
             break
-        print(player, enemy_boss)
     if player.health <= 0:
-        print(player, enemy_boss)
-        print(e_set)
         print('Part 2:', sum([x.cost for x in e_set]))
         break
